0f15e8fa50f6167399b3d9461ce647980258de3a
Mohammad Ali <ma.jamei@raymon.info>
2024-06-02 16:19:10 +0330

    wrap categories.

--- src/CategoryList/categoryList.jsx
+++ src/CategoryList/categoryList.jsx
@@ -1,20 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "../axios";
 
 const CategoryList = () => {
 
+  const [categories, setCategories] = useState([])
+
   useEffect(() => {
     const fetchCategories = async () => {
       const response = await axios.get('/FoodCategory/categories');
-      console.log('response: ', response.data)
+      setCategories(response.data)
     }
     fetchCategories()
   }, [])
 
   return (
-    <div className="">
-      Category List
-    </div>
+    <nav className="container mt-n5">
+      <div
+        className="d-flex align-items-center  bg-white rounded-3 shadow-lg py-4"
+        style={{ height: "80px" }}
+      >
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              همه فست فود ها
+            </a>
+          </li>
+          {
+            categories?.map((category) => (
+              <li className="nav-item" key={category.id}>
+                <a className="nav-link" href="#">
+                  {category.name}
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </nav>
   )
 }
 export default CategoryList;
